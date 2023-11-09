47c7911b7dddb8c14b536e1b547a2195866aa8ec
Vishal Kumar <57776597+VishyFishy02@users.noreply.github.com>
2023-11-09 13:19:17 -0500

    app is working

--- App/app.py
+++ App/app.py
@@ -1,5 +1,6 @@
-from shiny import App, ui
-
+from shiny import App, ui, reactive, render
+from src.modelFunctions import get_predictions
+import pandas as pd
 # Part 1: ui ----
 app_ui = ui.page_fluid(
     ui.panel_title("Somm Wine Reccomendation Engine", window_title="Somm"),
@@ -11,11 +12,33 @@ app_ui = ui.page_fluid(
                   autocomplete='off', 
                   spellcheck='true'),
     ui.input_action_button(id='submit', label='Submit'),
-    ui.output_table("reccomendation"),
+    ui.output_table("recommendation"),
 )
 # Part 2: server ----
+# coordinates inputs and outputs
 def server(input, output, session):
-    ...
+    output_table = reactive.Value(None)
+
+    @reactive.Effect
+    @reactive.event(input.submit)
+    def on_submit():
+        query = input.user_query()
+        if query:  # Ensure there is a query to process
+            # Call the get_predictions function
+            predictions_array = get_predictions(query)
+            # Convert the numpy array to a pandas DataFrame
+            predictions_df = pd.DataFrame(predictions_array, columns=["Description"])
+            output_table.set(predictions_df)
+        else:
+            # Return an empty DataFrame or None if there is no query
+            output_table.set(pd.DataFrame())
+    
+    @output
+    @render.table
+    def recommendation():
+        return output_table.get()
+
+
 
 # Combine into a shiny app.
 # Note that the variable must be "app".
--- src/modelFunctions.py
+++ src/modelFunctions.py
@@ -1,54 +1,58 @@
-import pandas as pd
+from pathlib import Path
 from sentence_transformers import SentenceTransformer
 import torch
 import math
 from tqdm import tqdm
 import faiss
 import numpy as np
+import pandas as pd
 
-wine = pd.read_csv('winemag-data-130k-v2.csv', encoding='utf-8', on_bad_lines='skip')
-wine.rename(columns={'Unnamed: 0': 'id'}, inplace=True)
+def load_embeddings_and_wine():
+    # Get the directory where this script is located
+    current_dir = Path(__file__).parent
+    # Construct the path to the CSV file
+    data_file = current_dir.parent / 'Data/Raw Data/winemag-data-130k-v2.csv'
+    # Read the CSV file
+    wine = pd.read_csv(data_file, encoding='utf-8', on_bad_lines='skip')
+    wine.rename(columns={'Unnamed: 0': 'id'}, inplace=True)
 
-# # Generating and Exporting Description Embeddings to a Tensor File
+    # # KNN Search with Faiss
+    vector_file = current_dir.parent / 'Data/description_embeddings.pt'
+    knn_vectors = torch.load(vector_file)
+    knn_labels = wine.id.astype(str).tolist()
 
-# This code was used to create the description embeddings tensor file.
-# You don't have to run it again.
-# It takes about an hour to run.
+    # Convert the data to NumPy arrays for use with faiss
+    vectors_np = knn_vectors.numpy()
 
-# # KNN Search with Faiss
+    # Determine the dimension of the vectors
+    dimension = vectors_np.shape[1]  # This represents the dimension of the vectors
 
-knn_vectors = torch.load("description_embeddings.pt")
-knn_labels = wine.id.astype(str).tolist()
+    # Build the Faiss index
+    index = faiss.IndexFlatL2(dimension)  # Create a Faiss index with L2 (Euclidean) distance metric
+    index.add(vectors_np)  # Add the data vectors to the index
+    
+    return wine, index 
 
-# Convert the data to NumPy arrays for use with faiss
-vectors_np = knn_vectors.numpy()
+def get_predictions(query_text):
+    # Encode the text query to obtain a vector
+    query_vector = model.encode([query_text])[0]
 
-# Determine the dimension of the vectors
-dimension = vectors_np.shape[1]  # This represents the dimension of the vectors
+    # Query the index with the vector
+    k = 10  # Number of neighbors to return
 
-# Build the Faiss index
-index = faiss.IndexFlatL2(dimension)  # Create a Faiss index with L2 (Euclidean) distance metric
-index.add(vectors_np)  # Add the data vectors to the index
+    # Perform a nearest neighbor search to find the closest neighbors to the query vector(s).
+    # D will contain squared L2 distances between the query vector and its neighbors.
+    # I will contain the indices of the nearest neighbors in the dataset.
+    D, I = index.search(np.array([query_vector]), k)
+
+    # Get labels of the neighbors
+    # neighbor_labels = [knn_labels[i] for i in I[0]]
+
+    # Extract rows from the wine DataFrame
+    result = wine.iloc[I[0]]
+
+    return result['description'].values
 
 # Initialize a sentence transformer model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-# Encode the text query to obtain a vector
-query_text = 'melon and bitter'
-query_vector = model.encode([query_text])[0]
-
-# Query the index with the vector
-k = 10  # Number of neighbors to return
-
-# Perform a nearest neighbor search to find the closest neighbors to the query vector(s).
-# D will contain squared L2 distances between the query vector and its neighbors.
-# I will contain the indices of the nearest neighbors in the dataset.
-D, I = index.search(np.array([query_vector]), k)
-
-# Get labels of the neighbors
-neighbor_labels = [knn_labels[i] for i in I[0]]
-
-# Extract rows from the wine DataFrame
-result = wine.iloc[I[0]]
-
-result['description'].values
+wine, index = load_embeddings_and_wine()
